c151c6d70d3e66f2b700cab7d5e28326baa1b189
ytimesy <yukio.juna1224@gmail.com>
2023-05-13 13:25:31 +0900

    model Controller routting

---
+++ db/migrate/20230513041149_create_conversations.rb
@@ -0,0 +1,8 @@
+class CreateConversations < ActiveRecord::Migration[6.1]
+  def change
+    create_table :conversations do |t|
+      t.references   :user, null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
